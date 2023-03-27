63ea00ccb68e1eaeceddcfe2591d4168785ca1e9
Jonathan Velez <110303080+Jav0918@users.noreply.github.com>
2023-03-27 16:30:46 -0500

    Initial Commit

---
+++ Be_Real_Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Be_Real_Clone
+//
+//  Created by Jonathan Velez on 3/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
